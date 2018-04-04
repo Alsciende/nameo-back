8bc2ceabb13d251e130f316e02e26d3d9ff281d3
Cédric Bertolini <c.bertolini@wakeonweb.com>
2018-04-04 17:04:47 +0200

    card fixtures

---
+++ tests/Distribution/DeterministicDistributionTest.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Tests\Distribution;
+
+use App\Distribution\DeterministicDistribution;
+
+class DeterministicDistributionTest extends \PHPUnit_Framework_TestCase
+{
+}
---
+++ tests/Distribution/RandomDistributionTest.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Tests\Distribution;
+
+use App\Distribution\RandomDistribution;
+
+class RandomDistributionTest extends \PHPUnit_Framework_TestCase
+{
+}
